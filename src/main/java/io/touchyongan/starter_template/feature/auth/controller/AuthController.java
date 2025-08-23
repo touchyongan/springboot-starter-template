82d86e98a0a4746c2b28515206970d28d7bbd8a6
TOUCH Yong An <yongan.touch@kmaker.io>
2025-08-24 02:15:42 +0700

    Add log entity and action

--- src/main/java/io/touchyongan/starter_template/feature/auth/controller/AuthController.java
+++ src/main/java/io/touchyongan/starter_template/feature/auth/controller/AuthController.java
@@ -1,9 +1,11 @@
 package io.touchyongan.starter_template.feature.auth.controller;
 
+import io.touchyongan.starter_template.feature.audit.aop.LogActionAnonymous;
 import io.touchyongan.starter_template.feature.auth.data.AuthResponseData;
 import io.touchyongan.starter_template.feature.auth.data.LoginRequest;
 import io.touchyongan.starter_template.feature.auth.data.RefreshTokenRequest;
 import io.touchyongan.starter_template.feature.auth.service.AuthenticationService;
+import io.touchyongan.starter_template.feature.user.data.AppUserConstant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +22,14 @@ public class AuthController {
     private final AuthenticationService authenticationService;
 
     @PostMapping(value = "/token", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @LogActionAnonymous(entity = AppUserConstant.ENTITY, action = AppUserConstant.LOGIN)
     public ResponseEntity<AuthResponseData> login(@Validated @RequestBody final LoginRequest request) {
         final var result = authenticationService.login(request);
         return ResponseEntity.ok(result);
     }
 
     @PostMapping(value = "/refresh", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @LogActionAnonymous(entity = AppUserConstant.ENTITY, action = AppUserConstant.REFRESH_TOKEN)
     public ResponseEntity<AuthResponseData> refreshToken(@Validated @RequestBody final RefreshTokenRequest request) {
         final var result = authenticationService.refreshToken(request);
         return ResponseEntity.ok(result);
---
+++ src/main/java/io/touchyongan/starter_template/feature/user/data/AppUserConstant.java
@@ -0,0 +1,13 @@
+package io.touchyongan.starter_template.feature.user.data;
+
+import io.touchyongan.starter_template.common.base.BaseConstant;
+
+public final class AppUserConstant extends BaseConstant {
+
+    private AppUserConstant() {
+    }
+
+    public static final String ENTITY = "APP_USER";
+    public static final String LOGIN = "LOGIN";
+    public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
+}
